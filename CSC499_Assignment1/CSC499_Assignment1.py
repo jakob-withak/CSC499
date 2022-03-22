93bdce5a47312093a8efb84ff5e49363d44e052d
Jakob Smith <jalexander.smith99@gmail.com>
2022-03-22 16:04:43 -0400

    Added reverse feature

--- CSC499_Assignment1/CSC499_Assignment1.py
+++ CSC499_Assignment1/CSC499_Assignment1.py
@@ -1,4 +1,8 @@
 # CSC 499 sorting app
+input = input("Enter r to reverse: ")
+reverse = False
+if(input == "r"):
+    reverse = True
 # open the file and load each name into a list
 reader = open("Sort Me.txt", "r")
 content = reader.read()
@@ -14,7 +18,7 @@ for i in range(len(list)):
 # sort by alphabetical order
 list.sort()
 # sort by length ascending
-list = sorted(list, key=len, reverse=False)
+list = sorted(list, key=len, reverse=reverse)
 
 for word in list:
     print(word)
