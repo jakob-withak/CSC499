7a5166f3dcf6f2f313874bb4a9297b8b4127839b
Jakob Smith <90628666+jakob-withak@users.noreply.github.com>
2022-04-12 19:03:19 -0400

    Update test.sh
    updated test.sh to throw an error if difference in files to make the actions file work easier.

--- Clean-Build/test.sh
+++ Clean-Build/test.sh
@@ -5,10 +5,12 @@ if diff -b  normal.txt normalout.txt; then
 	echo "Normal Pass"
 else
 	echo "Normal Fail"
+	exit 1 #terminate and throw error
 fi
 #test reverse
 if diff -b reversed.txt reversedout.txt; then
 	echo "Reverse Pass"
 else
 	echo "Reverse Fail"
+	exit 1 #terminate and throw error
 fi
